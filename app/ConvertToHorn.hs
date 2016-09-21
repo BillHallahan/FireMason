ed46dd7607bbe5f227f5a9a01c2158596c24540d
Bill Hallahan <billhallahan@juno.com>
2016-09-21 10:45:47 -0400

    Switched the way rules from the iptables scripts are split into multiple rules (introduce variables now.)

--- app/ConvertToHorn.hs
+++ app/ConvertToHorn.hs
@@ -1,4 +1,4 @@
-module ConvertToHorn where
+module ConvertToHorn (stringInputChainsToStringChains, inputInstructionsToInstructions) where
 
 import Types
 
@@ -6,20 +6,20 @@ stringInputChainsToStringChains :: [(String, InputChain)] -> Int -> [(String, Ch
 stringInputChainsToStringChains [] _ = []
 stringInputChainsToStringChains ((s, c):sc) j = 
     let
-        c' = inputChainToChain c j
+        c' = inputChainToChain (eliminateOrPropVar) c j
     in
     (s, c'):stringInputChainsToStringChains sc (j + length c')--Adding length c' is sufficient to ensure no
                                                                --collisions, but will skip some numbers, this is fine
 
-inputChainToChain :: [InputRule] -> Int -> [Rule]
-inputChainToChain [] _ = []
-inputChainToChain (r:rs) i = 
+inputChainToChain :: ([InputCriteria] -> Int -> ([[Criteria]], [Rule], Int)) -> [InputRule] -> Int -> [Rule]
+inputChainToChain _ [] _ = []
+inputChainToChain elim (r:rs) i = 
     let
-    (newC, newR, i') = inputCriteriaToCriteria (criteria r) i
+    (newC, newR, i') = inputCriteriaToCriteria (elim) (criteria r) i
     r' = map (\c -> Rule c (targets r) (label r)) newC
     newR' = map (\r'' -> Rule (criteria r'') (targets r'') (label r)) newR
     in
-    newR' ++ r' ++ (inputChainToChain rs (i + i' + length newR))
+    newR' ++ r' ++ (inputChainToChain (elim) rs (i + i' + length newR))
 
 --This should be called only on the [InputCriteria] in an And, and will combine multiple And's into one as much as possible
 condenseAnd :: [InputCriteria] -> [InputCriteria]
@@ -37,38 +37,49 @@ condenseOr (c:cx) = c:condenseOr cx
 --This should be called only on the [InputCriteria] in an Or
 eliminateOr :: [InputCriteria] -> Int -> ([[Criteria]], [Rule], Int)
 eliminateOr [] i = ([], [], i)
-eliminateOr (c:cx) i = 
+eliminateOr (c:cx) i =
     let
-        (c', r, i') = inputCriteriaToCriteria [c] i
+        (c', r, i') = inputCriteriaToCriteria (eliminateOr) [c] i
         (c'', r', i'') = eliminateOr cx i'
     in
     (c' ++ c'', r ++ r', i'')
 
-
-inputCriteriaToCriteria :: [InputCriteria] -> Int -> ([[Criteria]], [Rule], Int)
-inputCriteriaToCriteria [] i = ([[]], [], i)
-inputCriteriaToCriteria (And c: cx) i = inputCriteriaToCriteria ((condenseAnd c) ++ cx) i
-inputCriteriaToCriteria (Or c:cx) i = 
+eliminateOrPropVar :: [InputCriteria] -> Int -> ([[Criteria]], [Rule], Int)
+eliminateOrPropVar [] _ = ([], [], -1)
+eliminateOrPropVar cx i =
     let
-        (c', r, i') = eliminateOr (condenseOr $ c) i
-        (c'', r', i'') = inputCriteriaToCriteria cx (i')
+        (c', r, i') = inputCriteriaToCriteria (eliminateOrPropVar) cx i
+        c'' = concat c'
+        r' = map (\ct -> Rule [ct] [PropVariableTarget i' True] (-1)) c''
+        r'' = Rule (map (\ct -> Not ct) (c'')) [PropVariableTarget i' False] (-1)
+    in
+    ([[PropVariableCriteria i']], r'':r ++ r', i' + 1)
+
+inputCriteriaToCriteria :: ([InputCriteria] -> Int -> ([[Criteria]], [Rule], Int)) -> [InputCriteria] -> Int -> ([[Criteria]], [Rule], Int)
+inputCriteriaToCriteria _ [] i = ([[]], [], i)
+inputCriteriaToCriteria elim (And c: cx) i = inputCriteriaToCriteria (elim) ((condenseAnd c) ++ cx) i
+inputCriteriaToCriteria elim (Or c:cx) i = 
+    let
+        (c', r, i') = elim (condenseOr $ c) i
+        (c'', r', i'') = inputCriteriaToCriteria (elim) cx (i')
     in
     ((mappend) <$> c' <*> c'', r ++ r', i'')
-inputCriteriaToCriteria (InCNot (InC c):cx) i = 
+inputCriteriaToCriteria elim (InCNot (InC c):cx) i = 
     let
-        (c'', r', i') =  inputCriteriaToCriteria cx i
+        (c'', r', i') =  inputCriteriaToCriteria (elim) cx i
     in
     (map (Not c:) c'', r', i')
-inputCriteriaToCriteria (InCNot c:cx) i = inputCriteriaToCriteria ((simplifyNots [InCNot c]) ++ cx) i
-inputCriteriaToCriteria (c':cx) i = 
+inputCriteriaToCriteria elim (InCNot c:cx) i = inputCriteriaToCriteria elim ((simplifyNots [InCNot c]) ++ cx) i
+inputCriteriaToCriteria elim (c':cx) i = 
     let
         c = case c' of InC c -> c
                        InCLimit r b -> Limit i r b
-        (c'', r', i') =  inputCriteriaToCriteria cx i
+        (c'', r', i') =  inputCriteriaToCriteria elim cx i
         propRules = [Rule [c] [PropVariableTarget i True] (-1), Rule [Not c] [PropVariableTarget i False] (-1)]
     in
-    if isStateless c then (map (c:) c'', r', i')
-        else (map (PropVariableCriteria i:) c'', r' ++ propRules, i' + 1)
+    (map (c:) c'', r', i')--if isStateless c then (map (c:) c'', r', i')
+       -- else (map (PropVariableCriteria i:) c'', r' ++ propRules, i' + 1)
+
 
 --Moves all Nots as deep into the criteria as possible,
 --by applying DeMorgans Law
@@ -92,11 +103,11 @@ inputInstructionsToInstructions :: [InputInstruction] -> Int -> [Instruction]
 inputInstructionsToInstructions [] _ = []
 inputInstructionsToInstructions ((ToChainNamed s r):xs) i =
     let
-         rules = map (ToChainNamed s) (inputChainToChain [r] i)
+         rules = map (ToChainNamed s) (inputChainToChain (eliminateOr) [r] i)
     in
     rules ++ inputInstructionsToInstructions xs (i + length rules)
 inputInstructionsToInstructions ((NoInstruction r):xs) i =
     let
-        rules = map (NoInstruction) (inputChainToChain [r] i)
+        rules = map (NoInstruction) (inputChainToChain (eliminateOr) [r] i)
     in
     rules ++ inputInstructionsToInstructions xs (i + length rules)
